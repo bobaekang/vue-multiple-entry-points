7342cebb5e2ee40573a962839d6d90cb10f30866
bobaekang <bkang.dev@gmail.com>
2020-06-06 10:10:38 -0500

    Implement logic for multi entry point
    Inspired by: https://blog.logrocket.com/multiple-entry-points-in-create-react-app-without-ejecting/

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from "vue";
-import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
+const BUILD_TARGETS = [
+  {
+    name: "app",
+    path: "./App.vue"
+  },
+  {
+    name: "test",
+    path: "./Test.vue"
+  }
+];
+
+const { path } = BUILD_TARGETS.find(
+  ({ name }) => process.env.VUE_APP_BUILD_TARGET === name
+);
+
+import(`${path}`).then(({ default: App }) =>
+  new Vue({
+    render: h => h(App)
+  }).$mount("#app")
+);
